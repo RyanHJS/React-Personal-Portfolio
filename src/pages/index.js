038151b84979ef725bdaabc10ee0943b07c89941
heryanh <ryan_he@sfu.ca>
2023-04-10 22:22:58 -0700

    componentized

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ import { useState } from 'react';
 import Navbar from './Components/Navbar';
 import About from './Components/About';
 import Work from './Components/Work';
+import Projects from './Components/Projects';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -46,30 +47,7 @@ export default function Home() {
                     {/* My work Experience */}
                     <Work />
                     {/* Portofolio */}
-                    <div>
-                        <h3 className='text-3xl  font-medium pt-8 pb-2 dark:text-white'>My Projects</h3>
-                        <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-                            <div className="basis-1/3 flex-1 ">
-                                <Image
-                                    className="rounded-lg object-cover"
-                                    width={"100%"}
-                                    height={"100%"}
-                                    layout="responsive"
-                                    src={web1}
-                                />
-                            </div>
-                            <div className="basis-1/3 flex-1">
-                                <Image
-                                    className="rounded-lg object-cover"
-                                    width={"100%"}
-                                    height={"100%"}
-                                    layout="responsive"
-                                    src={web2}
-                                />
-                            </div>
-                        </div>
-                    </div>
-
+                    <Projects web1={web1} web2={web2} Image={Image} />
                 </section>
 
                 {/* Footer */}
