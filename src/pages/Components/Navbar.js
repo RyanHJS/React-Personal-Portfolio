43de69a7e0a7b23e4a1431879aac3d9416f7c34a
heryanh <ryan_he@sfu.ca>
2023-04-10 22:07:39 -0700

    added favicon

---
+++ src/pages/Components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { motion } from 'framer-motion';
+
+const Navbar = ({ darkMode, setDarkMode }) => {
+    return (
+        <>
+            <nav className="py-10 mb-12 flex justify-between dark:text-white">
+                <h1 className="font-burtons text-xl animate-bounce">Ryan He</h1>
+                <ul className="flex items-center">
+                    <li>
+                        <motion.div whileInView={{ opacity: 1 }} whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}>
+                            <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl" />
+                        </motion.div>
+                    </li>
+                    <li>
+                        <motion.div whileInView={{ opacity: 1 }} whileHover={{ scale: 1.1 }} transition={{ duration: 0.5, type: 'tween' }}>
+                            <a
+                                className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                                href="#"
+                            >
+                                Resume
+                            </a>
+                        </motion.div>
+                    </li>
+                </ul>
+            </nav>
+        </>
+
+    );
+};
+
+export default Navbar; 
---
+++ src/pages/Components/Work.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Work = () => {
+    return (
+        <>
+            <div >
+                <h3 className='text-3xl font-medium pt-8 pb-2 dark:text-white'>My Work Experience</h3>
+                <h4 className='text-md pt-10 pb-2 leading-8 tet-gray-80 dark:text-gray-200 '>
+                    SAP - Software Logistics Engineer (September 2021- May 2022)</h4>
+                <p className="text-md py-2 leading-8 text-gray-800 max-w-3xl dark:text-gray-200">I worked as a SAP iXp intern on the BTP Core Platform Services
+                    team where I conducted system analysis, testing, and <span className='text-teal-500'>debugging</span>.
+                    I also examined resource consumption in physical and <span className='text-teal-500'>virtual environments </span>
+                    (VMware, PowerVM, etc) and optimized resource usage and response times.</p>
+                <p className="text-md py-2 leading-8 text-gray-800 max-w-3xl dark:text-gray-200">I am honoured to have been nominated by my manager to
+                    join <span className='text-teal-500'>Fast Track</span> -  an initiative of SAP Internship Experience Program,
+                    for success, high motivation and commitment during internship/working student time.</p>
+            </div>
+        </>
+    );
+};
+
+export default Work;
