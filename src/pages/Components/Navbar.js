c835fb856c448574ca09c9d26c0bd97c4723dcee
heryanh <ryan_he@sfu.ca>
2023-04-12 17:01:48 -0700

    componentized navbar and made it mobile friendly

--- src/pages/Components/Navbar.js
+++ src/pages/Components/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { BsFillMoonStarsFill } from 'react-icons/bs';
 import { motion } from 'framer-motion';
-import Link from 'next/link';
+import NavLink from './NavLink';
+import { useState } from 'react';
+
 
 const Navbar = ({ darkMode, setDarkMode }) => {
     return (
@@ -10,56 +12,11 @@ const Navbar = ({ darkMode, setDarkMode }) => {
             bg-white px-10 dark:bg-gray-900 dark:text-white 
             py-8 mb-12
             top-0 fixed z-50 w-full">
+
                 <div className="container mx-auto flex items-center justify-between">
+                    <h1 className="font-burtons text-xl animate-bounce hidden sm:flex">Ryan He</h1>
 
-                    <h1 className="font-burtons text-xl animate-bounce">Ryan He</h1>
-                    <ul className="flex items-center">
-                        <li>
-                            <motion.div
-                                whileInView={{ opacity: 1 }}
-                                whileHover={{ scale: 1.1 }}
-                                transition={{ duration: 0.5, type: 'tween' }}
-                            >
-                                <a
-                                    className="px-4 py-2 rounded-md ml-8"
-                                    href='#about'
-                                >
-                                    About
-                                </a>
-                            </motion.div>
-                        </li>
-                        <li>
-                            <motion.div
-                                whileInView={{ opacity: 1 }}
-                                whileHover={{ scale: 1.1 }}
-                                transition={{ duration: 0.5, type: 'tween' }}
-                            >
-                                <a
-                                    className="px-4 py-2 rounded-md ml-8 "
-                                    href='#work'
-                                >
-                                    Work
-                                </a>
-                            </motion.div>
-                        </li>
-                        <li>
-                            <motion.div
-                                whileInView={{ opacity: 1 }}
-                                whileHover={{ scale: 1.1 }}
-                                transition={{ duration: 0.5, type: 'tween' }}
-                            >
-                                <a
-                                    className="px-4 py-2 rounded-md ml-8 "
-                                    href='#project'
-                                >
-                                    Project
-                                </a>
-                            </motion.div>
-                        </li>
-
-                    </ul>
-
-                    <ul className="flex items-center">
+                    <ul className="items-center hidden sm:flex">
                         <li>
                             <motion.div
                                 whileInView={{ opacity: 1 }}
@@ -69,20 +26,17 @@ const Navbar = ({ darkMode, setDarkMode }) => {
                                 <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl" />
                             </motion.div>
                         </li>
-                        <li>
-                            <motion.div
-                                whileInView={{ opacity: 1 }}
-                                whileHover={{ scale: 1.1 }}
-                                transition={{ duration: 0.5, type: 'tween' }}
-                            >
-                                <a
-                                    className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
-                                    href='/static/resume.pdf'
-                                >
-                                    Resume
-                                </a>
-                            </motion.div>
-                        </li>
+                        <NavLink anchorLink={'/static/resume.pdf'}
+                            anchorTitle={'Resume'}
+                            extraStyle={'bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white border-none'} />
+                    </ul>
+                </div>
+
+                <div className="container mx-auto flex items-center justify-center">
+                    <ul className="flex items-center justify-center">
+                        <NavLink anchorLink={'#about'} anchorTitle={'About'} />
+                        <NavLink anchorLink={'#work'} anchorTitle={'Work'} />
+                        <NavLink anchorLink={'#project'} anchorTitle={'Project'} />
                     </ul>
                 </div>
             </nav >
